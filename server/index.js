7bc843819efc24a371663ab09f08c3a24a882e3b
rajeshkhadka <rajeshkhadkaofficia@gmail.coml>
2024-07-04 17:05:42 +0545

    db connected ✅

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import { connectDB } from "./utils/db.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
