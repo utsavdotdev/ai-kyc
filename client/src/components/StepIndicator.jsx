79b2d7ecb991100503dd4976316182daea49538b
utsavbhattarai007 <ninja08469@gmail.com>
2024-07-12 07:49:41 +0545

    small tweaks

--- client/src/components/StepIndicator.jsx
+++ client/src/components/StepIndicator.jsx
@@ -1,6 +1,29 @@
 import React from "react";
 
 const StepIndicator = ({ currentStep }) => {
+
+  const steps = [{
+    id:1,
+    title:"Personal Info"
+  },
+  {
+    id:2,
+    title:"Address Info"
+  },
+  {
+    id:3,
+    title:"Photo Upload"
+  },
+  {
+    id:4,
+    title:"Docs Upload"
+  },
+  {
+    id:5,
+    title:"Review"
+  }
+
+]
   return (
     <div className="step-indicator">
       {[1, 2, 3, 4, 5].map((step, index) => (
